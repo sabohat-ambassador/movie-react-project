0720311fca22329db4c7f306c6fb2ea7235ac164
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-12-16 11:49:19 +0500

    fix production errors

--- src/componenets/GenresList.js
+++ src/componenets/GenresList.js
@@ -4,24 +4,20 @@ import { useState } from "react";
 import { useEffect } from "react/cjs/react.development";
 import apiCalls from "../config/api";
 
-export const genres = async () => {
-  try {
-      const data = await apiCalls.genres();
-      console.log(data)
-      setGenresLIst(data.genres);
-
-  } catch (error) {
-      setError(error.message);
-  }
- 
-}
-
 const Genres=()=>{
 
     const [genresList, setGenresLIst] = useState([]);
     const [error, setError] = useState('');
 
     useEffect(() => {
+      const genres = async () => {
+        try {
+          const data = await apiCalls.genres();
+          setGenresLIst(data.genres);
+        } catch (error) {
+          setError(error.message);
+        }
+      }
       genres();
     },[]);
 
